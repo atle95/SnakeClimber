5c320975e3f499ebf57e2b20c0bd2ef663f38ba8
Ben Matthews <Ben Matthews>
2017-02-25 19:32:43 -0700

    updated drawable to use a list of images

--- SnakeClimber/src/application/Model/Drawable.java
+++ SnakeClimber/src/application/Model/Drawable.java
@@ -2,6 +2,8 @@ package application.Model;
 
 import javafx.scene.image.Image;
 
+import java.util.ArrayList;
+
 /**
  * Created by benma on 2/25/2017.
  *
@@ -10,17 +12,41 @@ import javafx.scene.image.Image;
 public class Drawable extends GameObject{
     public double x, y, scale, angle;
 
-    public Image image;
+    /**
+     * the speed at which the images in the images arraylist cycle
+     */
+    public double ticksPerFrame;
 
-    public Drawable(Image image){
+    private int imageIndex, ticks;
+    public ArrayList<Image> images;
 
+    public Drawable(ArrayList<Image> images){
+        this(images, 1);
+    }
+
+    public Drawable(ArrayList<Image> images, double scale){
+        this(images, scale, 5);
+    }
+
+    public Drawable(ArrayList<Image> images, double scale, double ticksPerFrame){
+        this.images = images;
+        this.scale = scale;
+        this.ticksPerFrame = ticksPerFrame;
+
+        imageIndex = ticks = 0;
+        x = y = angle = 0;
     }
 
     public void tick(){
+        ticks++;
+        if (ticks > ticksPerFrame){
+            ticks = 0;
+            imageIndex = (imageIndex+1)%images.size();
+        }
 
     }
 
     public void render(){
-
+        
     }
 }
