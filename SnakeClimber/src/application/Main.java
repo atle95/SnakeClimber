e4f46bf1ec12394d6e3af4a3e1ebceaec1d2b0ed
atle95 <atle95@gmail.com>
2017-02-26 00:22:59 -0700

    Added some cool visual effect on a canvas

--- SnakeClimber/src/application/Main.java
+++ SnakeClimber/src/application/Main.java
@@ -2,6 +2,8 @@ package application;
 	
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Random;
 
 import javax.imageio.ImageIO;
 
@@ -20,10 +22,13 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
   
-  static int width  = 800;
-  static int height = 600;
+  static int width  = 1280;
+  static int height = 800;
   GraphicsContext gfx;
   ImageView imgv;
+  RandomLine randomLine;
+  ArrayList<RandomLine> randomLines = new ArrayList<>();
+  Canvas canvas;
   
 	@Override
 	public void start(Stage primaryStage) {
@@ -34,14 +39,21 @@ public class Main extends Application {
       WritableImage wimg = SwingFXUtils.toFXImage(bimg, null);
       imgv = new ImageView(wimg);
 
-      Canvas canvas = new Canvas(width, height);
+      canvas = new Canvas(3*width, 3*height);
 			BorderPane root = new BorderPane();
 			Scene scene = new Scene(root, width, height);
 
 			root.getChildren().add(canvas);
-			root.getChildren().add(imgv);
+//			root.getChildren().add(imgv);
 			gfx = canvas.getGraphicsContext2D();
 			gfx.setFill(Color.BLACK);
+			randomLine = new RandomLine(width/2, height/2, 10, 0, 10);
+			
+			for(int i = 0; i < 30; i++)
+			{
+			  RandomLine rl = new RandomLine(canvas.getWidth()/2, canvas.getHeight()/2, 20, 0, 20);
+			  randomLines.add(rl);
+			}
 			
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
@@ -57,18 +69,67 @@ public class Main extends Application {
 	class MainGameLoop extends AnimationTimer
   {
 	  int increment = 0;
+	  
 	  @Override
     public void handle(long now)
     {
+	    Random r = new Random();
+	    if( increment % 60 == 0)
+	    {
+	      randomLine = randomLines.get(r.nextInt(randomLines.size()));
+	    }
 	    increment++;
 	    gfx.setFill(Color.BLACK);
-      gfx.fillRect(width-increment, height-increment, width, height);
-      imgv.setTranslateX(increment);
-      imgv.setTranslateY(increment);
+	    gfx.setStroke(Color.BLACK);
+	    for(RandomLine rl : randomLines)
+	    {
+	      rl.tick();
+	      gfx.fillOval(rl.x, rl.y, rl.rad, rl.rad);
+	    }
+	    randomLine.trackOnCanvas();
+	    gfx.fillOval(randomLine.x, randomLine.y, randomLine.rad, randomLine.rad);
     }
   }
 	
-	public static void main(String[] args) {
+	class RandomLine
+	{
+	  double x, y, rad, angle, speed = 1;
+	  double range = Math.PI/6;
+	  double decayRate = 1.0/120;
+	  Random r = new Random();
+	  
+	  RandomLine(double x, double y, double rad, double angle, double speed)
+	  {
+	    this.x = x;
+	    this.y = y;
+	    this.rad = rad;
+	    this.angle = r.nextDouble()*Math.PI*2;
+	  }
+	  
+	  void tick()
+	  {
+	    double decay      = r.nextDouble()*decayRate;
+	    double deltaAngle = r.nextDouble()*range;
+	    double deltaX     = Math.cos(angle)*speed;
+	    double deltaY     = Math.sin(angle)*speed;
+	    
+	    rad -= decay;
+	    this.angle += deltaAngle-range/2;
+	    this.x += deltaX;
+	    this.y += deltaY;
+	  }
+	  
+	  void trackOnCanvas()
+	  {	    
+	    canvas.setTranslateX(width/2-this.x);
+	    canvas.setTranslateY(height/2-this.y);
+	  }
+	  
+	}
+	
+	public static void main(String[] args)
+	{
+	  
 		launch(args);
 	}
 }
