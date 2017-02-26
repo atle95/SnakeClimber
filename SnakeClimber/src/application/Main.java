925ddc2a9162e4ad8c1368d96a30a48e6ef78e04
atle95 <atle95@gmail.com>
2017-02-25 18:22:25 -0700

    Added resources folder and simple import code (for reference)

--- SnakeClimber/src/application/Main.java
+++ SnakeClimber/src/application/Main.java
@@ -1,17 +1,31 @@
 package application;
 	
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.WritableImage;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
+		  
+      File face = new File("resources/face.png");
+      BufferedImage bimg = ImageIO.read(face);
+      WritableImage wimg = SwingFXUtils.toFXImage(bimg, null);
+      ImageView imgv = new ImageView(wimg);
 			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			root.getChildren().add(imgv);
+			Scene scene = new Scene(root, 400, 400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
