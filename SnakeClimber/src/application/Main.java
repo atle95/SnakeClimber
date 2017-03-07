a97806700eb5397798e41545ca27d0dbf448d358
atle95 <atle95@gmail.com>
2017-03-07 15:47:57 -0700

    Added all the stuff

--- SnakeClimber/src/application/Main.java
+++ SnakeClimber/src/application/Main.java
@@ -29,8 +29,14 @@ public class Main extends Application {
   ImageView imgv;
   RandomLine randomLine;
   RandomLine trackingLine;
-  ArrayList<RandomLine> randomLines = new ArrayList<>();
+  public static ArrayList<RandomLine> randomLines = new ArrayList<>();
+  public static ArrayList<RandomLine> addingList = new ArrayList<>();
+  ArrayList<RandomLine> removalList = new ArrayList<>();
+  ArrayList<Color> colorList = new ArrayList<>();
   Canvas canvas;
+  Random r = new Random();
+  public double globalPrune = 0.75;
+  int numcolors = 10;
   
 	@Override
 	public void start(Stage primaryStage) {
@@ -44,17 +50,38 @@ public class Main extends Application {
       canvas = new Canvas(3*width, 3*height);
 			BorderPane root = new BorderPane();
 			Scene scene = new Scene(root, width, height);
+			
+			
+//			colorList = makePallet(numcolors);
 
 			root.getChildren().add(canvas);
 //			root.getChildren().add(imgv);
 			gfx = canvas.getGraphicsContext2D();
 			gfx.setFill(Color.BLACK);
-			for(int i = 0; i < 30; i++)
+			gfx.fillRect(0, 0, 3*width, 3*height);
+			int numTrees = 15;
+			int treeLength = 240;
+			double centerx = canvas.getWidth()/2-width/2;
+			double centery = canvas.getHeight()/2;
+			for(int i = 0; i < numTrees; i++)
 			{
-			  RandomLine rl = new RandomLine(canvas.getWidth()/2, canvas.getHeight()/2, 1, 0, 1, Color.BLACK);
-			  randomLines.add(rl);
+			  double progress = ((double) i) / numTrees;
+			  double xoffset = progress*width;
+			  int lifespan = (int) (progress*treeLength);
+			  
+			  RandomLine redTree   = new RandomLine(centerx+xoffset, centery-200);
+			  RandomLine greenTree = new RandomLine(centerx+xoffset, centery    );
+			  RandomLine blueTree  = new RandomLine(centerx+xoffset, centery+200);
+			  
+			  redTree  .setBranchRate(120+i).setRange(Math.PI/3 ).setLifespan(lifespan).setRad(i).setColor(Color.RED);
+			  greenTree.setBranchRate(60+i ).setRange(Math.PI/6 ).setLifespan(lifespan).setRad(i).setColor(Color.GREEN);
+			  blueTree .setBranchRate(30+i ).setRange(Math.PI/12).setLifespan(lifespan).setRad(i).setColor(Color.DODGERBLUE);
+			  
+			  randomLines.addAll(Arrays.asList(redTree, greenTree, blueTree ));
 			}
-			trackingLine = new RandomLine(canvas.getWidth()/2, canvas.getHeight()/2, 0, 0, 0, Color.BLACK);
+			randomLine = new RandomLine(canvas.getWidth()/2, canvas.getHeight()/2);
+//			randomLine.grow();
+			trackingLine = randomLine;
 			
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
@@ -70,34 +97,99 @@ public class Main extends Application {
 	class MainGameLoop extends AnimationTimer
   {
 	  int increment = 0;
-	  double spread = 1;
+//	  int branchRate = 240;
 	  
 	  @Override
     public void handle(long now)
     {
-//	    if( increment % 60 == 0)
+//	    if( randomLines.size() > 0)
 //	    {
-//	      trackingLine = randomLines.get((randomLines.indexOf(trackingLine)+1)%randomLines.size());
+//	      if( increment % 30 == 0)
+//	      {
+//	        trackingLine = randomLines.get((randomLines.indexOf(trackingLine)+1)%randomLines.size());
+//	      }
+//	    } else
+//	    {
+//	      trackingLine = randomLine;
 //	    }
+//	    cycleColors(colorList);
+	    
 	    increment++;
 	    for(RandomLine rl : randomLines)
 	    {
-	      rl.tick();
-	      gfx.setFill(rl.color);
-	      gfx.setStroke(rl.color);
-	      for(int i = -10; i < 10; i++)
+	      if(increment % rl.branchRate == 0)
 	      {
-	        int col = 255 - Math.abs(i)*255/10;
-	        gfx.setFill(Color.rgb(col, col, col) );
-	        gfx.fillOval(rl.x+Math.cos(rl.angle+Math.PI/2)*spread*i, rl.y+Math.sin(rl.angle+Math.PI/2)*spread*i, rl.rad, rl.rad);
-	        
+	        addingList.add(rl.branch());
 	      }
+	      
+	      if (rl.rad < globalPrune )
+	      {
+	        removalList.add(rl);
+	      }
+	      
+	      rl.tick();
+//	      for(double[] path : randomLine.paths)
+//	      {
+//	        gfx.setFill(Color.PURPLE);
+//	        gfx.setStroke(Color.PURPLE);
+//	        int samples = 5;
+//	        for(int i = -samples; i <= samples; i++)
+//	        {
+////	          int col = 255 - Math.abs(i)*255/samples;
+//	          int col = (int) (128*(1+Math.cos(i*Math.PI/samples)));
+//	          int red   = (int) ((rl.color.getRed()*255)+col)/2;
+//	          int green = (int) ((rl.color.getGreen()*255)+col)/2;
+//	          int blue  = (int) ((rl.color.getBlue()*255)+col)/2;
+//	          Color c = Color.rgb(red, green, blue);
+//	          gfx.setFill(c);
+//	          gfx.setStroke(c);
+//	          gfx.fillOval(
+//	              path[0]+Math.cos(path[3]+Math.PI/2)*path[2]*i,
+//	              path[1]+Math.sin(path[3]+Math.PI/2)*path[2]*i,
+//	              path[3],
+//	              path[3]);
+//	          
+//	        }
+//	        
+//	      }
+	      int samples = 5;
+        for(int i = -samples; i <= samples; i++)
+        {
+//          int col = 255 - Math.abs(i)*255/samples;
+          int col = (int) (128*(1+Math.cos(i*Math.PI/samples)));
+//          int col = (int) (255*Math.sqrt(samples^2-i^2)/samples);
+          
+          
+          rl.cycleColors();
+//          cycleColors(colorList);
+//          cycleColors(colorList);
+          Color c = rl.colorList.get(0);
+          
+          int red   = (int) ((c.getRed()  *255)+col)/2;
+          int green = (int) ((c.getGreen()*255)+col)/2;
+          int blue  = (int) ((c.getBlue() *255)+col)/2;
+          Color shadec = Color.rgb(red, green, blue, 0.95);
+          
+          gfx.setFill(shadec);
+          gfx.setStroke(shadec);
+
+          gfx.fillOval(
+              rl.x+Math.cos(rl.angle+Math.PI/2)*rl.rad*i/samples,
+              rl.y+Math.sin(rl.angle+Math.PI/2)*rl.rad*i/samples,
+              rl.rad/samples*2,
+              rl.rad/samples*2);
+          
+        }
+        
+//	    }
 	    }
+	    randomLines.addAll(addingList);
+	    randomLines.removeAll(removalList);
+	    addingList = new ArrayList<RandomLine>();
+	    removalList = new ArrayList<RandomLine>();
 	    trackingLine.trackOnCanvas(canvas);
     }
   }
-	
-	
 	
 	public static void main(String[] args)
 	{
--- SnakeClimber/src/application/Model/RandomLine.java
+++ SnakeClimber/src/application/Model/RandomLine.java
@@ -1,5 +1,6 @@
 package application.Model;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 import application.Main;
@@ -8,20 +9,42 @@ import javafx.scene.paint.Color;
 
 public class RandomLine
 {
-  public double x, y, rad, angle, speed;
-  public double range = Math.PI/12;
+  public double x, y, rad, angle, speed, lifespan;
   public Color color;
-  private double decayRate = 1.0/120;
+  public ArrayList <double[]> paths;
+  public ArrayList <Color> colorList;
+  public int branchRate;
+
+  private double range;
+  private double colorRange;
+  private double decayRate;
+  private double prune = 0.05;
   private Random r = new Random();
+  private ArrayList <RandomLine> branches;
   
-  public RandomLine(double x, double y, double rad, double angle, double speed, Color color)
+  public RandomLine(double x, double y, double rad, double angle, double speed, Color color, double lifespan)
   {
-    this.x = x;
-    this.y = y;
-    this.rad = rad;
-    this.angle = r.nextDouble()*Math.PI*2;
-    this.speed = speed;
-    this.color = color;
+    this.setX(x).setY(y).setRad(rad).setAngle(angle).setSpeed(speed).setColor(color).setLifespan(lifespan);
+    this.decayRate = rad/lifespan;
+    this.branches = new ArrayList<>();
+    this.paths = new ArrayList<>();
+    this.branchRate = 60+r.nextInt(60);
+    this.colorList = makePallet(20, r.nextDouble()*2*Math.PI);
+    this.colorRange = r.nextDouble()*Math.PI;
+  }
+  
+  public RandomLine(double x, double y)
+  {
+    this.setX(x).setY(y).setRad(1).setAngle(-Math.PI/2)
+        .setLifespan(100).setSpeed(1).setLifespan(100).setBranchRate(120)
+        .makePallet(20, r.nextDouble()*2*Math.PI);
+    this.decayRate = rad/lifespan;
+    this.branches = new ArrayList<>();
+    this.paths = new ArrayList<>();
+    this.branchRate = 120;
+    this.colorList = makePallet(20, r.nextDouble()*2*Math.PI);
+    this.colorRange = Math.PI*2/3;
+    
   }
   
   public void tick()
@@ -31,12 +54,129 @@ public class RandomLine
     double deltaX     = Math.cos(angle)*speed;
     double deltaY     = Math.sin(angle)*speed;
     
-//    rad -= decay;
+    rad = rad < prune ? 0 : rad-rad*decayRate;
     this.angle += deltaAngle-range/2;
     this.x += deltaX;
     this.y += deltaY;
+    paths.add(new double [] {x, y, rad, angle});
   }
   
+  public RandomLine setX(double x)
+  {
+    this.x = x;
+    return this;
+  }
+  
+  public RandomLine setY(double y)
+  {
+    this.y = y;
+    return this;
+  }
+  
+  public RandomLine setPrune(double prune)
+  { 
+    this.prune = prune;
+    return this;
+  }
+  
+  public RandomLine setBranchRate(int branchRate)
+  {
+    this.branchRate = branchRate;
+    return this;
+  }
+  
+  public RandomLine setColor(Color c)
+  {
+    this.color = c;
+    return this;
+  }
+  
+  public RandomLine setRange(double range)
+  {
+    this.range = range;
+    return this;
+  }
+  
+  public RandomLine setRad(double rad)
+  {
+    this.rad = rad;
+    return this;
+  }
+  
+  public RandomLine setLifespan(double lifespan)
+  {
+    this.lifespan = lifespan;
+    return this;
+  }
+  
+  public RandomLine setSpeed(double speed) 
+  {
+    this.speed = speed;
+    return this;
+  }
+  
+  public RandomLine setAngle(double angle)
+  {
+    this.angle = angle;
+    return this;
+  }
+  
+  public RandomLine setColorList(ArrayList<Color> colorList )
+  {
+    this.colorList = colorList;
+    return this;
+  }
+  
+  public void grow()
+  {
+    for(int i = 0; i < lifespan; i++)
+    {
+        this.tick();
+    }
+  }
+  
+  public RandomLine branch()
+  {
+    
+    RandomLine branch = new RandomLine(this.x, this.y);
+    
+    branch .setBranchRate(this.branchRate).setRange(this.range).setColor(this.color).
+    setRad(this.rad).setLifespan(this.lifespan).setPrune(this.prune).
+    setSpeed(this.speed).setAngle(this.angle).setColorList(this.colorList);
+    
+    double branchSplit = r.nextDouble();
+    this.angle   += branchSplit*range;
+    branch.angle -= branchSplit*range;
+    branches.add(branch);
+    return branch;
+  }
+  
+  public void cycleColors()
+  {
+    Color tempc = colorList.get(0);
+    colorList.remove(tempc);
+    colorList.add(tempc);
+  }
+  
+  ArrayList<Color> makePallet(int numcolors, double angle)
+  {
+    
+    ArrayList<Color> tempcolorList = new ArrayList<>();
+    double redOffset   = angle;
+    double greenOffset = Math.PI*2/3+redOffset;
+    double blueOffset  = -Math.PI*2/3+redOffset;
+    
+    for(int i = 0; i <= numcolors; i++)
+    {
+      double progress = ((double) i) / numcolors;
+      int r = (int) (255 * (( 1 + Math.sin(Math.PI*progress + redOffset  )) / 2));
+      int g = (int) (255 * (( 1 + Math.sin(Math.PI*progress + greenOffset)) / 2));
+      int b = (int) (255 * (( 1 + Math.sin(Math.PI*progress + blueOffset )) / 2));
+      tempcolorList.add(Color.rgb(r, g, b));
+    }
+    return tempcolorList;
+  }
+
   public void trackOnCanvas(Canvas canvas)
   {     
     canvas.setTranslateX(Main.width/2-this.x);
